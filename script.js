4b366a42056e55c97ae91f27253f03f7277cf111
e0026557 <e0026557@u.nus.edu>
2021-12-30 22:14:34 +0800

    Added JS for collapse button for mobile and iPad screen

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get collapse button
+  const collapseBtn = document.querySelector("#navbar-button");
+  // Get additional Navlinks
+  const addnavbar = document.querySelector(".navbar-text");
+
+  // switch variable to keep track of clicks
+  let click = true;
+
+  // Toggle additional navlinks to disappear when collapse button is clicked
+  collapseBtn.addEventListener("click", () => {
+    if (click === true) {
+      // console.log("turn off");
+      addnavbar.style.display = "none";
+      click = false;
+    } else {
+      // console.log("turn on");
+      addnavbar.style.display = "block";
+      click = true;
+    }
+  });
+});
